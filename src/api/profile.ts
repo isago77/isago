b4ffca09e3a049ba453254dd83d6fe2953a0d43e
MTtankkeo <ttankkeo112@gmail.com>
2025-07-05 20:37:04 +0900

    제 3자에 대한 사용자 정보 조회 API 수정

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -9,7 +9,7 @@ const ProfileRequest = z.object({
 
 // profile
 export const PROFILE_HANDLER = new HTTPHandler({
-    get: async (request, response, body) => {
+    get: async (request, response, _) => {
         const given = API.tryParseURL(ProfileRequest, API.urlOf(request));
 
         // 제 3자가 특정 사용자의 정보를 불러올 수 있는 최소한의 필드가 정의되어 있습니다.
@@ -26,9 +26,31 @@ export const PROFILE_HANDLER = new HTTPHandler({
             [given.uuid]
         );
 
-        // 유효하지 않은 사용자 UUID의 경우.
+        // 유효하지 않은 사용자 UUID인 경우.
         if (!row) throw APIError.INVALID_UUID;
 
-        API.success(response, row);
+        const userId = row.id;
+        let result = row;
+
+        // 조회된 역할에 따라 추가적으로 부가적인 사용자 정보를 조회해야 할 때.
+        if (result.role != null) {
+            const fields = [
+                `introduction`,
+                `bannerUrl`,
+                `links`,
+                `contactAs`,
+                `serviceAreas`,
+            ];
+
+            const [row] = await DB_CLIENT.query(
+                `SELECT ${fields.join(", ")} FROM UserDetails WHERE userId = ? LIMIT 1`,
+                [userId]
+            );
+
+            // 데이터 병합.
+            if (row) result = {...result, ...row};
+        }
+
+        API.success(response, result);
     }
 });
