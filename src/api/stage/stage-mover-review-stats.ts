575b95d3fb84be5dae4336d53c51f55583f110e0
MTtankkeo <ttankkeo112@gmail.com>
2025-07-16 01:39:10 +0900

    리뷰에 대한 평점 분포 데이터 통계 추가

--- src/api/stage/stage-mover-review-stats.ts
+++ src/api/stage/stage-mover-review-stats.ts
@@ -17,19 +17,38 @@ export const STAGE_MOVER_REVIEW_STATS_HANDLER = new HTTPHandler({
             "AVG(a.rating) AS averageRating",
         ]
 
-        const [row] = await DB_CLIENT.query(
-            `
-                SELECT ${fields.join(", ")} FROM MoverReview a
-                JOIN MoverStage b ON b.stageId = a.stageId
-                JOIN MoverRequest c ON c.id = b.requestId
-                WHERE moverId = ?
-            `,
-            [given.moverId]
-        );
+        const [[result], distributions] = await Promise.all([
+            // 전체 리뷰 수와 평균 평점을 조회합니다.
+            DB_CLIENT.query(
+                `
+                    SELECT ${fields.join(", ")} FROM MoverReview a
+                    JOIN MoverStage b ON b.stageId = a.stageId
+                    JOIN MoverRequest c ON c.id = b.requestId
+                    WHERE moverId = ?
+                `,
+                [given.moverId]
+            ),
+
+            // 평점별 리뷰 개수를 조회합니다. (0~5점)
+            DB_CLIENT.query(
+                `
+                    SELECT a.rating, COUNT(*) AS count
+                    FROM MoverReview a
+                    JOIN MoverStage b ON b.stageId = a.stageId
+                    JOIN MoverRequest c ON c.id = b.requestId
+                    WHERE c.moverId = ?
+                    GROUP BY a.rating
+                `,
+                [given.moverId]
+            ),
+        ]);
 
         // 문자열로 정의된 평균 평점 값을 숫자로 변환.
-        row.averageRating = parseFloat(row.averageRating);
+        result.averageRating = parseFloat(result.averageRating);
 
-        API.success(response, row);
+        // 평점 분포 데이터 정의.
+        result.distributions = distributions;
+
+        API.success(response, result);
     }
 });
